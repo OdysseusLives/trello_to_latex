3b8f8734406f8d0a6796927c3c594c0a2629c866
Aubrey Chipman <aubrey.chipman@gmail.com>
2013-03-27 17:14:00 -0500

    Basic json parsing when reading from string and reading from simple file

---
+++ parse_json.rb
@@ -0,0 +1,18 @@
+require "rubygems"
+require "json"
+
+string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
+parsed = JSON.parse(string) # returns a hash
+
+p parsed["desc"]["someKey"]
+p parsed["main_item"]["stats"]["a"]
+
+# Read JSON from a file, iterate over objects
+file = open("one_card_section.json")
+json = file.read
+
+parsed = JSON.parse(json)
+@tags = JSON.parse( json )[ "cards" ]
+@tags.each do |item| 
+  puts item["id"]
+end

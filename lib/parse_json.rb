680ee414c86360d816cc136683281f23a97746ff
Aubrey Chipman <aubrey.chipman@gmail.com>
2013-03-27 18:58:04 -0500

    Removed parsing from string, added reference for colored labels, looking at labelNames; still working with only one partial card.

--- lib/parse_json.rb
+++ lib/parse_json.rb
@@ -1,19 +1,22 @@
 require "rubygems"
 require "json"
 
-string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
-parsed = JSON.parse(string) # returns a hash
-
-p parsed["desc"]["someKey"]
-p parsed["main_item"]["stats"]["a"]
-
 # Read JSON from a file, iterate over objects
 file = open("input/one_card_section.json")
 json = file.read
 
 parsed = JSON.parse(json)
-@tags = JSON.parse( json )[ "cards" ]
+@tags = JSON.parse( json )["cards"]
 @tags.each do |item| 
-  puts "All items: #{item}"
-  puts "Only the id: #{item["id"]}"
+  puts "Card name: #{item["name"]}"
+  # puts "All items: #{item}"
+  puts "URL: #{item["url"]}"
+end
+
+@colored_labels = JSON.parse(json)["labelNames"]
+puts "\nReference for colored labels:"
+@colored_labels.each do |key, value| 
+	if value != "" then 
+		puts "The color #{key} indicates #{value}"
+	end
 end
